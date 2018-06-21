38e238c6c2c8acb325b6bfaade06edb930e0e315
Adel Lassag <adetech.aadl@gmail.com>
2018-06-21 05:10:50 +0100

    add other charachters and selector images into Resources

--- js/engine.js
+++ js/engine.js
@@ -184,6 +184,11 @@ const Engine = ((global => {
         'images/grass-block.png',
         'images/enemy-bug.png',
         'images/char-boy.png',
+        'images/char-cat-girl.png',
+        'images/char-horn-girl.png',
+        'images/char-pink-girl.png',
+        'images/char-princess-girl.png',
+        'images/Selector.png',
         'images/Heart.png',
         'images/Star.png'
     ]);
